d92cf342c3103c5d0cb56a93b307acfd2592cc4a
Grzegorz Kulesza <gkulesza@9bits.pl>
2018-05-14 15:28:42 +0200

    Data load

---
+++ MyApp/MyApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MyApp
+//
+//  Created by Grzesiek Kulesza on 14.05.2018.
+//
+
+import Foundation
+
+class Item {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+
