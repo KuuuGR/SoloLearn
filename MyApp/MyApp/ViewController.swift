5cf7dc79fb5758c098ee010c3a46c6a6449a57b9
Grzegorz Kulesza <gkulesza@9bits.pl>
2018-05-14 10:32:34 +0200

    Add references to swift class

--- MyApp/MyApp/ViewController.swift
+++ MyApp/MyApp/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
