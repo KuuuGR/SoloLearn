26adb29bd731057f4969e1e7a72eba8076505d9d
Grzegorz Kulesza <gkulesza@9bits.pl>
2018-05-17 11:13:38 +0200

    Define the Data Model finished

---
+++ MyApp/MyApp/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  MyApp
+//
+//  Created by Grzesiek Kulesza on 17.05.2018.
+//
+
+import Foundation
+
+class Item {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
